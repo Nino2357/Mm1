65ba40e8d2f12c6f44e5b72a830ec268e1d721d9
Nino <nino.delien@gmail.com>
2020-04-02 12:25:34 +0200

    debut de l'ecriture de la foction de codage

---
+++ codeur.c
@@ -0,0 +1,14 @@
+
+
+
+//Provisoirement dans ce dossier
+int main(void)
+{
+	return 0;
+}
+
+
+void initialisation(void)
+{
+	
+}
---
+++ codeur.h
@@ -0,0 +1,15 @@
+
+
+
+
+typedef struct reglages_partie
+{
+	int nb_trous;
+	int nb_couleurs;
+	int nb_tour_max;
+}*Reg_Par;
+
+typedef struct rangee
+{
+	int 
+}
